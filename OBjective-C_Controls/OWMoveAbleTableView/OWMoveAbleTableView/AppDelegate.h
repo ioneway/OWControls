c02a4871e99aa7a499bf72a14e1dec9a4723b802
oneway <373850874@qq.com>
2016-03-27 19:27:13 +0800

    初始化

---
+++ OBjective-C_Controls/OWMoveAbleTableView/OWMoveAbleTableView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OWMoveAbleTableView
+//
+//  Created by weiwang on 16/3/27.
+//  Copyright © 2016年 王伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OBjective-C_Controls/OWMoveAbleTableView/OWMoveAbleTableView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OWMoveAbleTableView
+//
+//  Created by weiwang on 16/3/27.
+//  Copyright © 2016年 王伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
